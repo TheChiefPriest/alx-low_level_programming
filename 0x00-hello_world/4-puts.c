5e36c0cd12c7174bcff843701686d4448cd03cfc
TheChiefPriest <jaymaximum221@gmail.com>
2023-05-21 05:26:19 -0700

    Correction made to question 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'using the puts function to output a statement'
+ * @puts: 
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("Programming is lke building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
